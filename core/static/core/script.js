d6104c94ec317297f40b10bb8e31947ad62d8888
MicaelTargino <micaelgomestargino@gmail.com>
2024-07-10 20:41:23 -0300

    about section - improved text and styling

--- core/static/core/script.js
+++ core/static/core/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed('.typing', {
-    strings: ['','Web developer',  'Fullstack dev', 'Computer Science Student',],
+    strings: ['Software Engineer', 'Web developer', 'Data Engineer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
